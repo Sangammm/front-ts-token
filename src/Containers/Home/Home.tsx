7faf49da06e0ca305fb970b39c091b369ea6e730
Sangam Rajpara <sangam.r@simformsolutions.com>
2020-01-24 20:25:13 +0530

    hahahaha

--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../Components/Header/Header'
 import { useQuery } from '@apollo/react-hooks'
 import { GET_USERS } from '../../Apollo/gql'
@@ -26,22 +26,41 @@ interface returnType {
 }
 
 const Home: React.SFC<LoginProps> = () => {
-	const { loading, error, data } = useQuery<returnType>(GET_USERS)
+	const [users, setUsers] = useState()
+	const { loading, error, data } = useQuery<returnType>(GET_USERS, {
+		onCompleted: (data: returnType) => {
+			if (data.users) {
+				setUsers(data.users.users)
+			}
+		},
+	})
+
+	const moveup = (item: User, i: number) => {
+		if (data) {
+			let usersClone = [...users]
+			usersClone.splice(0, 0, item)
+			console.log(usersClone, 1)
+			usersClone.splice(i + 1, 1)
+			console.log(usersClone, 1)
+			setUsers(usersClone)
+		}
+	}
 	return (
 		<>
 			<Header links={[]} logout />
+			{loading && <Loader />}
 			<div className="home">
 				<p className="title">Usersssss</p>
-				{loading && <Loader />}
 				{error && (error.graphQLErrors[0].message || 'Something went wrong')}
 				<div className="homeContainer">
 					{data &&
 						data.users &&
-						data.users.users.map((item: User, i: number) => {
+						users &&
+						users.map((item: User, i: number) => {
 							return (
-								<div className="infoContainer" key={item.id}>
+								<div className="infoContainer" key={item.id} onClick={() => moveup(item, i)}>
 									<div className="centerContent">
-										<img className="profile" src={`https://picsum.photos/id/${10 + i}/200/200`} alt="profile" />
+										{/* <img className="profile" src={`https://picsum.photos/id/${10 + item.id}/200/200`} alt="profile" /> */}
 										<div className="nameEmail">
 											<p className="name">{item.name}</p>
 											<p className="email">{item.email}</p>
